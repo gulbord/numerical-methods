06c431134fbb4b73f4bac4f0c48162c0bf8716b3
gulbord <52690645+gulbord@users.noreply.github.com>
2023-11-05 23:06:27 +0100

    Create first two plots and save them to svg

---
+++ src/plots.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+theme_set(theme_minimal(base_family = "STIX Two Text", base_size = 9))
+my_blue <- "#002E63"
+my_red <- "#9E2B25"
+
+# A01b_disk_hit_miss
+read_tsv("../out/A01b.txt", col_names = c("throws", "error")) |>
+    mutate(error = 100 * error) |>
+    ggplot(aes(throws, error)) +
+        geom_line(linewidth = 0.2, colour = my_blue) +
+        labs(x = "Number of throws", y = "Error (%)")
+
+ggsave(
+    "../tex/img/A01b.svg",
+    width = 10, height = 7.5, unit = "cm",
+)
+
+# A02a_inversion_method
+x <- seq(0, 1, length.out = 5000)
+fx <- 4 * x^3
+smp <- readBin("../out/A02a_3.txt", what = "double", size = 8, n = 1e5)
+ggplot() +
+    geom_histogram(
+        aes(smp, after_stat(density), fill = "hist"),
+        bins = 80,
+        boundary = 0,
+        alpha = 0.5,
+        linewidth = 0,
+    ) +
+    geom_line(aes(x, fx, colour = "pdf")) +
+    scale_fill_manual(values = my_blue, labels = "Simulated data") +
+    scale_colour_manual(values = my_blue, labels = "PDF") +
+    labs(x = expression(italic("x")), y = expression(italic("ρ(x)"))) +
+    theme(
+        legend.title = element_blank(),
+        legend.position = c(0.2, 0.8),
+        legend.spacing.y = unit(0, "pt"),
+    )
+
+ggsave(
+    "../tex/img/A02a_3.svg",
+    width = 10, height = 7.5, unit = "cm",
+)
+
