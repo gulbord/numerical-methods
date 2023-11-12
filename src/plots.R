b421fa17b5d72345acccdb6e9f3d43499f51c85b
gulbord <52690645+gulbord@users.noreply.github.com>
2023-11-12 19:26:46 +0100

    Importance sampling plot instructions

--- src/plots.R
+++ src/plots.R
@@ -177,3 +177,93 @@ ggsave(
     "../tex/img/A03cb.svg",
     width = 10, height = 7.5, unit = "cm",
 )
+
+# A04a_gauss_crude_vs_import
+smp <- readBin("../out/A04a.txt", what = "double", size = 8, n = 201 * 450) |>
+    matrix(ncol = 201, byrow = TRUE)
+smp[, -1] <- 100 * abs(1 - smp[, -1] / 0.25)
+smp <- as.data.frame(smp) |>
+    rename_with(~ "n", 1) |>
+    rowwise() |>
+    mutate(
+        m_crude = mean(c_across(2:101)),
+        sd_crude = sd(c_across(2:101)),
+        m_impor = mean(c_across(102:201)),
+        sd_impor = sd(c_across(102:201)),
+    ) |>
+    select(n, ends_with("crude"), ends_with("impor")) |>
+    pivot_longer(
+        cols = -1,
+        names_to = c(".value", "method"),
+        names_pattern = "(.*)_(.*)",
+    )
+ggplot(smp, aes(n, m)) +
+    geom_line(aes(colour = method), linewidth = 0.1) +
+    geom_ribbon(aes(fill = method, ymin = m - sd, ymax = m + sd), alpha = 0.5) +
+    scale_colour_manual(
+        values = c(my_red, my_blue),
+        labels = c("Crude", "Importance sampling"),
+    ) +
+    scale_fill_manual(
+        values = c(my_red, my_blue),
+        labels = c("Crude", "Importance sampling"),
+    ) +
+    labs(
+        x = "Number of iterations",
+        y = "Error (%)",
+        colour = "Monte Carlo method",
+        fill = "Monte Carlo method",
+    ) +
+    theme(
+        legend.position = c(0.7, 0.8),
+        # remove spacing between labels, but not between title and first label
+        legend.text = element_text(margin = margin(t = 0, unit = "pt")),
+    )
+
+ggsave(
+    "../tex/img/A04a.svg",
+    width = 10, height = 7.5, unit = "cm",
+)
+
+# A04b_cosx_importance
+smp <- readBin("../out/A04b.txt", what = "double", size = 8, n = 100 * 101) |>
+    matrix(ncol = 101, byrow = TRUE)
+smp[, -1] <- 100 * abs(1 - smp[, -1])
+smp <- as.data.frame(smp) |>
+    rename_with(~ "n", 1) |>
+    rowwise() |>
+    mutate(m = mean(c_across(-1)), sd = sd(c_across(-1))) |>
+    select(n, m, sd)
+ggplot(smp, aes(n, m)) +
+    geom_line(colour = my_blue, linewidth = 0.1) +
+    geom_ribbon(
+        aes(ymin = m - sd, ymax = m + sd),
+        fill = my_blue,
+        alpha = 0.5,
+    ) +
+    scale_x_continuous(breaks = scales::pretty_breaks()) +
+    labs(x = "Number of iterations", y = "Error (%)")
+
+ggsave(
+    "../tex/img/A04b.svg",
+    width = 10, height = 7.5, unit = "cm",
+)
+
+# A04c
+T <- seq(1, 20, length.out = 5000)
+var_rho <- function(t) sqrt(exp(t) - 1)
+var_g <- function(t) {
+    a <- (1 + t - sqrt(1 + t^2)) / t
+    return(sqrt(exp(a * t) / (a * (2 - a)) - 1))
+}
+ggplot() +
+    geom_line(
+        aes(T, var_rho(T) / var_g(T)), colour = my_blue, linewidth = 0.1,
+    ) +
+    labs(x = expression(italic("T")), y = "Variance ratio")
+
+ggsave(
+    "../tex/img/A04c.svg",
+    width = 8, height = 6, unit = "cm",
+)
+
