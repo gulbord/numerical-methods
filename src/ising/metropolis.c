d68dcae36052a51a96257022456a19080a0e7b78
gulbord <52690645+gulbord@users.noreply.github.com>
2023-11-18 13:08:58 +0100

    Write evolution function and correct index access to nbrs

--- src/ising/metropolis.c
+++ src/ising/metropolis.c
@@ -1,4 +1,5 @@
 #include "metropolis.h"
+#include <math.h>
 
 static double comp_energy(Lattice* lat)
 {
@@ -8,12 +9,13 @@ static double comp_energy(Lattice* lat)
     for (i = 0; i < lat->side; ++i) {
         for (j = 0; j < lat->side; ++j) {
             // sum the up and right nearest neighbours
-            nn_sum = lat.spins[lat.nbrs[k]] + lat.spins[lat.nbrs[k + 1]]
+            nn_sum = lat.spins[lat.nbrs[4 * k]] + lat.spins[lat.nbrs[4 * k + 1]]
             e += lat.spins[k++] * nn_sum;
         }
     }
 
-    return (double)e;
+    // divide by N = L^2 to get average per spin
+    return -(double)e / (lat->side * lat->side);
 }
 
 static double comp_magnet(Lattice* lat)
@@ -24,18 +26,46 @@ static double comp_magnet(Lattice* lat)
         for (j = 0; j < lat->side; ++j)
             m += lat.spins[k++]; 
 
-    // divide by N = L^2 (average magnetization)
+    // divide by N = L^2 to get average per spin
     return (double)m / (lat->side * lat->side);
 }
 
 
-void evolve(Lattice* lat, int n_steps, double beta)
+void evolve(Lattice* lat, int n_steps, double beta, FILE* out_file)
 {
     double* energy = malloc(n_steps * sizeof(double));
     double* magnet = malloc(n_steps * sizeof(double));
 
-    int t, i, j, k;
-    for (t = 0; t < n_steps; ++t) {
-        
+    // compute energy and magnetization at initialization
+    energy[0] = comp_energy(lat);
+    magnet[0] = comp_magnet(lat);
+
+    int n_spins = lat->side * lat->side;
+    int t, pick, nn_sum, delta_e;
+    for (t = 1; t < n_steps; ++t) {
+        // pick a spin at random
+        pick = (int)(genrand_real2() * n_spins);
+
+        // compute the delta in energy from the spin flip
+        nn_sum = 0;
+        for (i = 0; i < 4; ++i)
+            nn_sum += lat.spins[lat.nbrs[4 * pick + i]];
+        delta_e = 2 * lat.spins[pick] * nn_sum;
+
+        // metropolis acceptance condition
+        if (delta_e < 0 || genrand_real1() < exp(-beta * delta_e)) {
+            lat.spins[pick] = -lat.spins[pick]; // flip spin
+            energy[t] = energy[t - 1] + (double)delta_e / n_spins;
+            magnet[t] = magnet[t - 1] + 2.0 * lat.spins[pick] / n_spins;
+        } else {
+            energy[t] = energy[t - 1];
+            magnet[t] = magnet[t - 1];
+        }
     }
+
+    fwrite(energy, n_steps * sizeof(double), n_steps, out_file);
+    fwrite(magnet, n_steps * sizeof(double), n_steps, out_file);
+
+    free(energy);
+    free(magnet);
 }
