b7bdb9d36bde12c923a9c9f94787cff84a311ef8
gulbord <52690645+gulbord@users.noreply.github.com>
2023-11-29 10:55:01 +0100

    Simplify loops in metropolis.c

--- src/ising/metropolis.c
+++ src/ising/metropolis.c
@@ -5,23 +5,19 @@
 static void comp_em(struct lattice *lat, int t, double *energy, double *magnet)
 {
     int e = 0, m = 0;
-    int nn_sum;
-    int i, j, n, k = 0;
-    for (i = 0; i < lat->side; ++i) {
-        for (j = 0; j < lat->side; ++j) {
-            // sum the up and right nearest neighbours
-            n = 4 * k;
-            nn_sum = lat->spins[lat->nbrs[n]] + lat->spins[lat->nbrs[n + 1]];
-            e -= lat->spins[k] * nn_sum;
-            m += lat->spins[k];
-            ++k;
-        }
+    int k, nn_sum;
+    int n_spins = lat->side * lat->side;
+    for (int i = 0; i < n_spins; ++i) {
+        // sum the up and right nearest neighbours
+        k = 4 * i;
+        nn_sum = lat->spins[lat->nbrs[k]] + lat->spins[lat->nbrs[k + 1]];
+        e -= lat->spins[i] * nn_sum;
+        m += lat->spins[i];
     }
 
     // divide by N = L^2 to get average per spin
-    double n_spins = lat->side * lat->side;
-    energy[t] = e / n_spins;
-    magnet[t] = m / n_spins;
+    energy[t] = e / (double)n_spins;
+    magnet[t] = m / (double)n_spins;
 }
 
 void evolve(struct lattice *lat, int n_steps, double beta,
@@ -33,29 +29,23 @@ void evolve(struct lattice *lat, int n_steps, double beta,
     int n_spins = lat->side * lat->side;
     int nn_sum, delta_e;
     double diff_e, diff_m; // to keep track of e and m between samplings
-    int t, i, j, k, n;
+    int t, i, j;
     for (t = 1; t < n_steps; ++t) {
-        n = 0;
         diff_e = 0.0;
         diff_m = 0.0;
         // visit all spins sequentially
-        for (i = 0; i < lat->side; ++i) {
-            for (j = 0; j < lat->side; ++j) {
-                // compute the delta in energy from the spin flip
-                nn_sum = 0;
-                for (k = 0; k < 4; ++k)
-                    nn_sum += lat->spins[lat->nbrs[4 * n + k]];
-                delta_e = 2 * lat->spins[n] * nn_sum;
-
-                // metropolis acceptance condition
-                if (delta_e < 0 || genrand_real1() < exp(-beta * delta_e)) {
-                    lat->spins[n] *= -1;
-                    diff_e += (double)delta_e / n_spins;
-                    diff_m += 2.0 * lat->spins[n] / n_spins;
-                }
-
-                // update counter and move to the next spin
-                ++n;
+        for (i = 0; i < n_spins; ++i) {
+            // compute the delta in energy from the spin flip
+            nn_sum = 0;
+            for (j = 4 * i; j < 4 * (i + 1); ++j)
+                nn_sum += lat->spins[lat->nbrs[j]];
+            delta_e = 2 * lat->spins[i] * nn_sum;
+            
+            // metropolis acceptance condition
+            if (delta_e < 0 || genrand_real1() < exp(-beta * delta_e)) {
+                lat->spins[i] *= -1;
+                diff_e += (double)delta_e / n_spins;
+                diff_m += 2.0 * lat->spins[i] / n_spins;
             }
         }
 
