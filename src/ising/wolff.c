722dcc27a5e6b24e0b6ee20ba2c1decd6260798c
gulbord <52690645+gulbord@users.noreply.github.com>
2023-12-02 16:25:32 +0100

    Working version of Wolff

--- src/ising/wolff.c
+++ src/ising/wolff.c
@@ -1,19 +1,20 @@
 #include "wolff.h"
 #include "../../lib/mt19937ar.h"
 #include <math.h>
+#include <string.h>
 
-void evolve(struct lattice *lat, int n_flips, double beta,
-            double *energy, double *magnet, int *clus_sz)
+void wolff(struct lattice *lat, int n_steps, double beta,
+           double *energy, double *magnet, int *clus_sz)
 {
     int n_spins = lat->side * lat->side;
     int e = 0, m = 0;
-    int i, j, nn_sum;
+    int i, n, nn_sum;
 
     // compute energy and magnetization at initialization
     for (i = 0; i < n_spins; ++i) {
         // sum the up and right nearest neighbours
-        j = 4 * i;
-        nn_sum = lat->spins[lat->nbrs[j]] + lat->spins[lat->nbrs[j + 1]];
+        n = 4 * i;
+        nn_sum = lat->spins[lat->nbrs[n]] + lat->spins[lat->nbrs[n + 1]];
         e -= lat->spins[i] * nn_sum;
         m += lat->spins[i];
     }
@@ -23,42 +24,76 @@ void evolve(struct lattice *lat, int n_flips, double beta,
     magnet[0] = (double)m / n_spins;
 
     double p_add = 1 - exp(-2 * beta);
-    int t, s, b, cs, front, rear, nbr;
+    int t, cs, front, rear; // counters
+    int seed, s0, nbr, delta_e;
+    // save unvisited spins in a queue
+    int unvisited[n_spins];
+    // array of bools for cluster membership
+    int is_cluster[n_spins];
 
-    for (t = 1; t < n_flips; ++t) {
+    for (t = 1; t < n_steps; ++t) {
+        // reset cluster membership
+        memset(is_cluster, 0, sizeof(is_cluster));
+
         // pick a starting spin at random
-        i = (int)(genrand_real1() * n_spins);
-        // flip the seed spin
-        s = lat->spins[i];
-        lat->spins[i] = -s;
+        seed = (int)(genrand_real2() * n_spins);
+        is_cluster[seed] = 1;
+        // flip the seed spin and save its value
+        s0 = lat->spins[seed];
+        lat->spins[seed] = -s0;
 
-        f = 0, r = 0; 
-        int unvis[n_spins]; // save unvisited spins in a queue
-        unvis[rear++] = i;
+        front = 0; rear = 0;
+        unvisited[rear++] = seed;
 
-        cs = 1; // cluster size counter
-        b = 0;  // number of border spins with different sign
-        while (f != r) {
+        cs = 1; // cluster size
+        while (front < rear) {
             // get an unvisited site from the front
-            i = unvis[front++];
-            // loop over neighbours 
-            for (j = 4 * i; j < 4 * (i + 1); ++j) {
-                ++b;
-                nbr = lat->nbrs[j];
-                // flip if right sign with probability p_add
-                if (lat->spins[nbr] == s) {
-                    --b;
-                    if (genrand_real1() < p_add) {
-                        lat->spins[nbr] = -s;
-                        unvis[rear++] = nbr; // enqueue the site to check it later
-                        ++cs;
-                    }
+            i = unvisited[front++];
+            // loop over neighbours
+            for (n = 4 * i; n < 4 * (i + 1); ++n) {
+                nbr = lat->nbrs[n];
+                // flip if right sign, with probability p_add
+                if (lat->spins[nbr] == s0 && genrand_real1() < p_add) {
+                    is_cluster[nbr] = 1;
+                    lat->spins[nbr] = -s0;
+                    // enqueue the site to check it later
+                    unvisited[rear++] = nbr;
+                    ++cs;
                 }
             }
         }
 
-        clus_sz[t - 1] = cs;
-        energy[t] = energy[t - 1] - 2.0 * b / n_spins;
-        magnet[t] = magnet[t - 1] - 2.0 * s * cs / n_spins;
+        clus_sz[t - 1] = cs; // clus_sz should have (n_steps - 1) elements!
+        magnet[t] = magnet[t - 1] - 2.0 * s0 * cs / n_spins;
+
+        // compute the energy difference
+        delta_e = 0;
+        front = 0; rear = 0;
+        unvisited[rear++] = seed;
+        while (front < rear) {
+            // get an unvisited cluster site from the front
+            i = unvisited[front++];
+            // loop over neighbours
+            for (n = 4 * i; n < 4 * (i + 1); ++n) {
+                nbr = lat->nbrs[n];
+
+                // c | b     c | b    [c = cluster, b = border]
+                // + | - --> + | +    then delta_e < 0
+                // + | + --> + | -    then delta_e > 0
+                switch (is_cluster[nbr]) {
+                    case 0: // border spin
+                        delta_e += s0 * lat->spins[nbr];
+                        break;
+                    case 1: // cluster spin
+                        is_cluster[nbr] += 1;
+                        unvisited[rear++] = nbr;
+                        break;
+                    default: // cluster spin, but already considered
+                        break;
+                }
+            }
+        }
+
+        energy[t] = energy[t - 1] + 2.0 * delta_e / n_spins;
     }
 }
--- src/ising/wolff.h
+++ src/ising/wolff.h
@@ -3,7 +3,7 @@
 
 #include "lattice.h"
 
-void evolve(struct lattice *lat, int n_flips, double beta,
-            double *energy, double *magnet, int *clus_sz)
+void wolff(struct lattice *lat, int n_flips, double beta,
+           double *energy, double *magnet, int *clus_sz);
 
 #endif
