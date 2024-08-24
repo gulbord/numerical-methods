9556e6b108c48506da4860db51009b10e6a329b3
gulbord <52690645+gulbord@users.noreply.github.com>
2024-08-24 23:47:00 +0200

    Add argument parsing for micro-canonical molecular dynamics code

---
+++ src/mcmd/parameters.c
@@ -0,0 +1,96 @@
+#include "parameters.h"
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int parse_config(const char *filename, struct parameters *params)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("fopen() failed");
+        return 1;
+    }
+
+    // Provide default values
+    params->num_particles = -1;
+    params->box_size = -1;
+    params->density = -1;
+    params->temperature = -1;
+    params->max_disp = -1;
+    params->step_size = 0.01;
+    params->num_steps = 1000;
+    params->num_eq_steps = 1000;
+    params->num_realizations = 1;
+    strcpy(params->init_conf, "random");
+    strcpy(params->eq_type, "md");
+
+    // Read line by line and split key-value pairs by whitespace
+    char line[LINE_BUFSIZ];
+    while (fgets(line, sizeof(line), file)) {
+        char key[TOKEN_BUFSIZ], value[TOKEN_BUFSIZ];
+
+        if (sscanf(line, "%s %s", key, value) != 2)
+            continue;
+
+        if (strcmp(key, "num_particles") == 0)
+            params->num_particles = atoi(value);
+        else if (strcmp(key, "box_size") == 0)
+            params->box_size = atof(value);
+        else if (strcmp(key, "density") == 0)
+            params->density = atof(value);
+        else if (strcmp(key, "temperature") == 0)
+            params->temperature = atof(value);
+        else if (strcmp(key, "max_disp") == 0)
+            params->max_disp = atof(value);
+        else if (strcmp(key, "step_size") == 0)
+            params->step_size = atof(value);
+        else if (strcmp(key, "num_steps") == 0)
+            params->num_steps = atoi(value);
+        else if (strcmp(key, "num_eq_steps") == 0)
+            params->num_eq_steps = atoi(value);
+        else if (strcmp(key, "num_realizations") == 0)
+            params->num_realizations = atoi(value);
+        else if (strcmp(key, "init_conf") == 0)
+            strcpy(params->init_conf, value);
+        else if (strcmp(key, "eq_type") == 0)
+            strcpy(params->eq_type, value);
+    }
+
+    if (params->temperature < 0.0) {
+        fprintf(stderr, "Provide a valid temperature value.\n");
+        fclose(file);
+        return 1;
+    }
+
+    if (params->num_eq_steps > 0 && params->max_disp < 0.0) {
+        fprintf(stderr, "Provide a valid max_disp value.\n");
+        fclose(file);
+        return 1;
+    }
+
+    // Complete num_particles, box_size and density
+    double volume;
+    int provided = (params->num_particles > 0) + (params->box_size > 0.0) +
+                   (params->density > 0.0);
+    if (provided < 2) {
+        fprintf(stderr, "Two values between num_particles, box_size, and "
+                        "density need to be provided!\n");
+        fclose(file);
+        return 1;
+    }
+
+    if (params->num_particles < 0) { // Calculate num_particles
+        volume = pow(params->box_size, 3);
+        params->num_particles = round(params->density * volume);
+    } else if (params->box_size < 0.0) { // Calculate box_size
+        volume = params->num_particles / params->density;
+        params->box_size = cbrt(volume);
+    } else { // Calculate density
+        volume = pow(params->box_size, 3);
+        params->density = params->num_particles / volume;
+    }
+
+    fclose(file);
+
+    return 0;
+}
---
+++ src/mcmd/parameters.h
@@ -0,0 +1,23 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#define LINE_BUFSIZ 256
+#define TOKEN_BUFSIZ 64
+
+struct parameters {
+    int num_particles;
+    double box_size;
+    double density;
+    double temperature;
+    double max_disp;
+    double step_size;
+    int num_steps;
+    int num_eq_steps;
+    int num_realizations;
+    char init_conf[TOKEN_BUFSIZ];
+    char eq_type[TOKEN_BUFSIZ];
+};
+
+int parse_config(const char *filename, struct parameters *params);
+
+#endif
