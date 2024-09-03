f9c2db9b50befc1c5c6e92c0a65401ace6e077f6
gulbord <52690645+gulbord@users.noreply.github.com>
2024-09-03 19:02:17 +0200

    Run for autocorrelation of Wolff observables

---
+++ src/061b_wolff_autocorr.R
@@ -0,0 +1,73 @@
+setwd("~/PoD/Y2.1/NMSM/exercises")
+source("src/preamble.R")
+
+acf_fft <- function(x, max_lag = NULL, thr = 0) {
+  x <- x - mean(x)
+
+  # Pad with zeros to correct cyclicity
+  len <- length(x)
+  x_pad <- c(x, rep.int(0L, len))
+  
+  # fft and inverse
+  fft <- fftwtools::fftw(x_pad)
+  acf <- fftwtools::fftw(abs(fft)^2, inverse = 1)
+
+  # Do the same on a 'mask' of ones to estimate the error
+  mask <- fftwtools::fftw(c(rep.int(1L, len), rep.int(0L, len))) 
+  err <- fftwtools::fftw(abs(mask)^2, inverse = 1)
+
+  # Normalize with error and variance
+  acf <- Re(acf / err)
+  var <- acf[1]
+
+  if (!is.null(max_lag))
+    return(acf[min(max_lag, len)] / var)
+
+  acf <- acf / var
+
+  return(acf[1:which.max(acf < thr)])
+}
+
+lat_sides <- round(exp(seq(log(10), log(50), length.out = 6)))
+num_steps <- 1e6L
+Tc <- 2 / log(1 + sqrt(2))
+
+# Already run
+# for (L in lat_sides) {
+#   argv <- sprintf(
+#     "exe/061_wolff %s%d %d %f %d",
+#     "acor_L", L, L, Tc, num_steps
+#   )
+#   system(argv)
+# }
+
+eqtime <- 500L # Manual analysis
+
+get_tau <- function(L) {
+  df <- fread(paste0("out/061_acor_L", L, ".csv")) |>
+    _[eqtime:.N] |>
+    _[, let(energy = energy / L^2, magnet = magnet / L^2)]
+  
+  avg_cs <- mean(df$clus_size)
+
+  lapply(
+    df[, .(energy, magnet)],
+    function(x) {
+        acf <- acf_fft(x)
+        tau_data <- sum((1 - seq_along(acf) / (nrow(df) - eqtime)) * acf)
+        return(tau_data * avg_cs / L^2)
+    }
+  )
+}
+
+taus <- lapply(lat_sides, get_tau) |>
+  rbindlist() |>
+  _[, lat_side := lat_sides]
+
+taus |>
+  melt(id.vars = "lat_side") |>
+  ggplot(aes(lat_side, value, colour = variable)) +
+    geom_point() +
+    geom_smooth(method = "lm", formula = y ~ x) +
+    scale_x_log10() +
+    scale_y_log10()
