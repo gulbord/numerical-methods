8c96664ec1a2ef153f2a7b370f24e8939c50b68c
gulbord <guglielmo@gmail.com>
2024-09-15 18:32:29 +0200

    Begin finite size scaling analysis for 5.2

--- src/051_ising_metropolis.R
+++ src/051_ising_metropolis.R
@@ -109,6 +109,7 @@ observ <- fread("src/051_results.csv") |>
     )
   ) |>
   _[level == 95] |>
+  _[, level := NULL] |>
   _[, names(.SD) := lapply(.SD, \(x) x * side^2 / temp),
     .SDcols = c("var", "low", "high")] |>
   _[obs == "energy", names(.SD) := lapply(.SD, \(x) x / temp),
@@ -150,18 +151,20 @@ plot_tex("051a", plt, asp_ratio = 0.75, scale_factor = 0.75)
 # Another set of simulations for a bigger energy/magnetization plot
 temps_new <- seq(0.2, 4.2, by = 0.1)
 {
-  sidetemp_new <- list()
+  pars_new <- list()
   k <- 1
   for (s in sides) {
     for (t in temps_new) {
-      sidetemp_new[[k]] <- list(side = s, temp = t)
+      pars_new[[k]] <- list(side = s, temp = t)
       k <- k + 1
     }
   }
 }
 
-launch_sim <- function(x) {
-  fname <- sprintf("L%d_T%g", x$side, x$temp)
+launch_sim <- function(x, prefix = "") {
+  if (prefix != "")
+    prefix <- paste0(prefix, "_")
+  fname <- sprintf("%sL%d_T%g", prefix, x$side, x$temp)
   system(
     sprintf(
       "exe/051_ising_metropolis %s %d %f %d",
@@ -170,14 +173,14 @@ launch_sim <- function(x) {
   )
 }
 
-# parallel::mclapply(sidetemp_new, launch_sim, mc.cores = getDTthreads())
+# parallel::mclapply(pars_new, launch_sim, mc.cores = getDTthreads())
 
 fnames_new <- lapply(
-  sidetemp_new,
+  pars_new,
   \(x) sprintf("out/051_L%d_T%g.csv", x$side, x$temp)
 )
 
-observ <- purrr::map(
+observ_new <- purrr::map(
   fnames_new,
   function(fname) {
     side <- as.integer(str_extract(fname, "(?<=L)\\d+"))
@@ -211,7 +214,7 @@ observ <- purrr::map(
 ) |>
   rbindlist()
 
-ggplot(observ, aes(temp, mean)) +
+plt_new <- ggplot(observ_new, aes(temp, mean)) +
   geom_pointrange(
     aes(ymin = low, ymax = high, colour = factor(side)),
     size = 0.004,
@@ -228,8 +231,8 @@ ggplot(observ, aes(temp, mean)) +
   ) +
   scale_colour_viridis_d() +
   scale_x_continuous(
-    breaks = c(pretty(observ$temp), Tc),
-    labels = c(pretty(observ$temp), "<i>T</i><sub>c</sub>"),
+    breaks = c(pretty(observ_new$temp), Tc),
+    labels = c(pretty(observ_new$temp), "<i>T</i><sub>c</sub>"),
   ) +
   labs(
     x = "Temperature",
@@ -240,3 +243,55 @@ ggplot(observ, aes(temp, mean)) +
     legend.position = "bottom",
     axis.text.x = ggtext::element_markdown(),
   )
+
+plot_tex("051b", plt_new, asp_ratio = 0.75, scale_factor = 0.75)
+
+# Finite size scaling analysis
+spec_heat <- observ[obs == "energy", .(side, temp, value = var)]
+tcrits <- spec_heat[, .(temp = temp[which.max(value)]), by = side]
+
+{
+  pars_fss <- list()
+  k <- 1
+  dt <- unique(diff(temps))[1]
+  for (i in seq_along(sides)) {
+    for (tc in round(tcrits[i, temp], 3)) {
+      temps_fss <- seq(tc - dt, tc + dt, by = dt / 5)
+      for (t in temps_fss) {
+        pars_fss[[k]] <- list(side = sides[i], temp = t)
+        k <- k + 1
+      }
+    }
+  }
+}
+
+# parallel::mclapply(
+#   pars_fss,
+#   \(x) launch_sim(x, prefix = "fss"),
+#   mc.cores = getDTthreads()
+# )
+
+fnames_fss <- lapply(
+  pars_fss,
+  \(x) sprintf("out/051_fss_L%d_T%g.csv", x$side, x$temp)
+)
+
+observ_fss <- purrr::map(
+  fnames_fss,
+  function(fname) {
+    side <- as.integer(str_extract(fname, "(?<=L)\\d+"))
+    temp <- as.numeric(str_extract(fname, "(?<=T)\\d+\\.?\\d*"))
+
+    energy <- fread(fname)[(eq_time + 1):.N, energy / side^2]
+    N <- length(energy)
+
+    acf <- acf_fft(energy, max_lag = 250, thr = 0.005)
+    tau <- sum((1 - seq_along(acf) / N) * acf)
+
+    spec_heat <- var(energy[seq(1, N, by = round(tau))]) * (side / temp)^2
+    
+    return(list(side = side, temp = temp, spec_heat = spec_heat))
+  },
+  .progress = TRUE
+) |>
+  rbindlist()
