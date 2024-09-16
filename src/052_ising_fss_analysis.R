276427adf05e406338a5d3c9ceb7a93d9395820f
gulbord <guglielmo@gmail.com>
2024-09-16 19:11:14 +0200

    Plot specific heat as a function of L around the maximum

--- src/052_ising_fss_analysis.R
+++ src/052_ising_fss_analysis.R
@@ -38,45 +38,87 @@ ggplot(spec_heat, aes(temp, value, colour = factor(side))) +
   theme(axis.text.x = ggtext::element_markdown())
 
 # Zoom in on the peaks and perform another set of simulations
-num_reps <- 10
 pars <- data.table(
   side = c(90L, 64L, 45L, 32L),
   low = c(2.26, 2.26, 2.27, 2.27),
-  high = c(2.29, 2.29, 2.31, 2.32)
-) |>
-  _[, .(temp = seq(low, high, length.out = 10)), keyby = side] |>
-  _[rep(seq_len(.N), each = num_reps)] |>
-  _[, rep := 1:.N, by = .(side, temp)]
+  high = c(2.29, 2.30, 2.31, 2.32)
+)[, .(temp = seq(low, high, length.out = 10)), keyby = side]
 
-split(pars, seq_len(nrow(pars))) |>
-  parallel::mclapply(
-    \(x) launch_sim(x[, !"rep"], prefix = paste0("fss", x$rep)),
-    mc.cores = min(10, parallel::detectCores())
+# split(pars, seq_len(nrow(pars))) |>
+#   parallel::mclapply(
+#     \(x) launch_sim(x, prefix = "fss", num_steps = 5e6),
+#     mc.cores = min(10, parallel::detectCores())
+#   )
+
+eq_steps <- 10000
+# fss_spec_heat <- pars |>
+#   _[, sprintf("out/051_fss_L%d_T%g.csv", side, temp)] |>
+#   parallel::mclapply(
+#     function(fname) {
+#       side <- as.integer(str_extract(fname, "(?<=L)\\d+"))
+#       temp <- as.numeric(str_extract(fname, "(?<=T)\\d+\\.?\\d*"))
+# 
+#       energy <- fread(fname)[(eq_steps + 1):.N, energy / side^2]
+# 
+#       acf <- acf_fft(energy, max_lag = 250, thr = 0.005)
+#       tau <- sum((1 - seq_along(acf) / length(energy)) * acf)
+# 
+#       result <- var(energy) * (1 + 2 * tau) * (side / temp)^2
+# 
+#       return(list(side = side, temp = temp, spec_heat = result))
+#     },
+#     mc.cores = getDTthreads()
+#   ) |>
+#   rbindlist()
+
+fss_spec_heat <- fread("src/data/052_fss_spec_heat.csv")
+
+tcrits <- split(fss_spec_heat, by = "side") |>
+  lapply(
+    function(x) {
+      fit <- lm(spec_heat ~ poly(temp, 2), x)
+      opt <- optimize(
+        \(t) predict(fit, list(temp = t)),
+        interval = c(min(x$temp), max(x$temp)),
+        maximum = TRUE
+      )
+      names(opt) <- c("temp", "spec_heat")
+      return(opt)
+    }
+  ) |>
+  rbindlist(idcol = "side")
+
+plt_tcrit <- ggplot(
+  fss_spec_heat,
+  aes(temp, spec_heat, group = factor(side))
+) +
+  geom_point(aes(colour = factor(side)), size = 0.75) +
+  geom_smooth(
+    aes(colour = factor(side), fill = factor(side)),
+    method = "lm",
+    formula = y ~ poly(x, 2),
+    alpha = 0.25,
+    linewidth = 0.5
+  ) +
+  geom_point(
+    data = tcrits,
+    shape = 23,
+    size = 1,
+  ) +
+  geom_text(
+    aes(label = sprintf("%.3f", temp)),
+    data = tcrits,
+    nudge_y = -12,
+    family = "TeX Gyre Pagella",
+    size = 7 / .pt
+  ) +
+  scale_colour_viridis_d() +
+  scale_fill_viridis_d() +
+  labs(
+    x = "Temperature",
+    y = "Specific heat per spin",
+    colour = "Lattice size",
+    fill = "Lattice size",
   )
 
-fnames <- split(pars, seq_len(nrow(pars))) |>
-  lapply(\(x) sprintf("out/051_fss%d_L%d_T%g.csv", x$rep, x$side, x$temp)) |>
-  unlist(use.names = FALSE)
-
-eq_steps <- 5000
-rowSds <- function(x, ...) {
-  sqrt(rowSums((x - rowMeans(x, ...))^2, ...) / (dim(x)[2] - 1))
-}
-
-
-fss_spec_heat <- pars[, sprintf("out/051_fss%d_L%d_T%g.csv", rep, side, temp) |>
-         lapply(\(x) fread(x)[(eq_steps + 1):.N, energy / side^2]) |>
-         lapply(
-           function(x) {
-             N <- length(x)
-             acf <- acf_fft(x, max_lag = 250, thr = 0.005)
-             tau <- sum((1 - seq_along(acf) / N) * acf)
-             return(var(x[seq(1, N, by = round(tau))]) * (side / temp)^2)
-           }
-         )
-     , by = .(side, temp)] |>
-  _[, .(side, temp, mean = rowMeans(.SD), sd = rowSds(.SD))
-    , .SDcols = !c("side", "temp")]
-
-ggplot(fss_spec_heat, aes(temp, mean, colour = factor(side))) +
-  geom_pointrange(aes(ymin = mean - sd, ymax = mean + sd))
+plot_tex("052a", plt_tcrit, asp_ratio = 5 / 3, scale_factor = 0.9)
