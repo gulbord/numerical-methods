5f948f8d657c48fc609ecd2ea07121c235f6fecd
gulbord <guglielmo@gmail.com>
2024-09-17 15:42:31 +0200

    Change PRNG once again: now it's MRG63k3a by Pierre L'Ecuyer

--- src/utils/random.c
+++ src/utils/random.c
@@ -2,96 +2,108 @@
 #include <float.h>
 #include <math.h>
 
-// The rng() is adapted from the MT19937-64 “Mersenne Twister” pseudorandom
-// number generator (29/09/2004 version), by Takuji Nishimura and Makoto
-// Matsumoto. The original source code is available at
-// http://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/VERSIONS/C-LANG/mt19937-64.c
+// 63-bits Random number generator U(0, 1): MRG63k3a
+// Author: Pierre L’Ecuyer
+// Source: P. L’Ecuyer, “Good Parameter Sets for Combined Multiple Recursive
+//         Random Number Generators,” Operations Research, vol. 47, no. 1, pp.
+//         159-164, Jan. 1999.
 
-#define NN 312
-#define MM 156
-#define MATRIX_A 0xB5026F5AA96619E9ULL
-#define UM 0xFFFFFFFF80000000ULL // Most significant 33 bits
-#define LM 0x7FFFFFFFULL         // Least significant 31 bits
-#define S 6364136223846793005ULL
+#define NORM 1.0842021724855052e-19
+#define M1 9223372036854769163
+#define M2 9223372036854754679
+#define A12 1754669720
+#define Q12 5256471877
+#define R12 251304723
+#define A13N 3182104042
+#define Q13 2898513661
+#define R13 394451401
+#define A21 31387477935
+#define Q21 293855150
+#define R21 143639429
+#define A23N 6199136374
+#define Q23 1487847900
+#define R23 985240079
 
-// Array for the state vector
-static uint64_t mt[NN];
-// mti == NN + 1 means mt[NN] is not initialized
-static int mti = NN + 1;
+static int64_t s[6];
+
+// Adapted from the SplitMix64 implementation by Sebastiano Vigna
+// Source: https://prng.di.unimi.it/splitmix64.c (2015)
+static uint64_t rng_next_seed(uint64_t seed)
+{
+    uint64_t x = (seed += 0x9e3779b97f4a7c15);
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+}
 
 void rng_set_seed(uint64_t seed)
 {
-    mt[0] = seed;
-    for (mti = 1; mti < NN; ++mti)
-        mt[mti] = S * (mt[mti - 1] ^ (mt[mti - 1] >> 62)) + mti;
+    s[0] = seed % M1;
+    s[1] = rng_next_seed(s[0]) % M1;
+    s[2] = rng_next_seed(s[1]) % M1;
+    s[3] = rng_next_seed(s[2]) % M2;
+    s[4] = rng_next_seed(s[3]) % M2;
+    s[5] = rng_next_seed(s[4]) % M2;
 }
 
-// Generate a random 64 bit unsigned int
-uint64_t rng(void)
+// Generate a random double on [0, 1)
+double rng_real(void)
 {
-    int i;
-    uint64_t x;
-    static uint64_t mag01[2] = {0ULL, MATRIX_A};
+    int64_t h, p12, p13, p21, p23;
 
-    if (mti >= NN) { // Generate NN words at one time
-        // if rng_set_seed() has not been called,
-        // a default initial seed is used
-        if (mti == NN + 1)
-            rng_set_seed(5489ULL);
+    // Component 1
+    h = s[0] / Q13;
+    p13 = A13N * (s[0] - h * Q13) - h * R13;
+    h = s[1] / Q12;
+    p12 = A12 * (s[1] - h * Q12) - h * R12;
+ 
+    if (p13 < 0)
+        p13 += M1;
+    if (p12 < 0)
+        p12 += M1 - p13;
+    else
+        p12 -= p13;
+    if (p12 < 0)
+        p12 += M1;
 
-        for (i = 0; i < NN - MM; ++i) {
-            x = (mt[i] & UM) | (mt[i + 1] & LM);
-            mt[i] = mt[i + MM] ^ (x >> 1) ^ mag01[(int)(x & 1ULL)];
-        }
-        for (; i < NN - 1; ++i) {
-            x = (mt[i] & UM) | (mt[i + 1] & LM);
-            mt[i] = mt[i + MM - NN] ^ (x >> 1) ^ mag01[(int)(x & 1ULL)];
-        }
+    s[0] = s[1];
+    s[1] = s[2];
+    s[2] = p12;
 
-        x = (mt[NN - 1] & UM) | (mt[0] & LM);
-        mt[NN - 1] = mt[MM - 1] ^ (x >> 1) ^ mag01[(int)(x & 1ULL)];
+    // Component 2
+    h = s[3] / Q23;
+    p23 = A23N * (s[3] - h * Q23) - h * R23;
+    h = s[5] / Q21;
+    p21 = A21 * (s[5] - h * Q21) - h * R21;
 
-        mti = 0;
-    }
+    if (p23 < 0)
+        p23 += M2;
+    if (p21 < 0)
+        p21 += M2 - p23;
+    else
+        p21 -= p23;
+    if (p21 < 0)
+        p21 += M2;
 
-    x = mt[mti++];
+    s[3] = s[4];
+    s[4] = s[5];
+    s[5] = p21;
 
-    x ^= (x >> 29) & 0x5555555555555555ULL;
-    x ^= (x << 17) & 0x71D67FFFEDA60000ULL;
-    x ^= (x << 37) & 0xFFF7EEE000000000ULL;
-    x ^= (x >> 43);
-
-    return x;
-}
-
-// Generate a random int (32 bit unsigned) on [0, n - 1]
-//
-// M.E. O'Neill. 2018. Efficiently generating a number in a range. PCG, a Better
-// Random Number Generator. Retrieved August 2024 from
-// https://www.pcg-random.org/posts/bounded-rands.html.
-uint32_t rng_int_n(uint32_t n)
-{
-    uint32_t mask = -1;
-    --n;
-    // Restrict mask to the closest [0, 2^k) range fitting [0, --n]
-    mask >>= __builtin_clz(n | 1);
-    uint32_t x;
-    do {
-        x = (rng() >> 32) & mask;
-    } while (x > n);
-
-    return x;
+    // Combination
+    if (p12 > p21)
+        return (p12 - p21) * NORM;
+    else
+        return (p12 - p21 + M1) * NORM;
 }
 
 // Generate an integer (32 bit signed) on [min, max]
 int32_t rng_int_range(int32_t min, int32_t max)
 {
-    uint32_t range = (uint32_t)(max - min) + 1U;
-    return min + rng_int_n(range);
+    return min + (int32_t)((max - min + 1.0) * rng_real());
 }
 
-// Generate a random double on [0, 1)
-double rng_real(void) { return (rng() >> 11) * 0x1.0p-53; }
+// Generate a random int (32 bit unsigned) on [0, n - 1]
+uint32_t rng_int_n(uint32_t n) { return (int32_t)(n * rng_real()); }
 
 // Generate a random double with a (0, 1) Gaussian distribution
 double rng_gauss(void)
