140d6d560a2b058272805a8465abcccdd569a88b
gulbord <guglielmo@gmail.com>
2024-09-17 11:46:22 +0200

    Go back to Mersenne Twister as PRNG

--- src/utils/random.c
+++ src/utils/random.c
@@ -2,50 +2,64 @@
 #include <float.h>
 #include <math.h>
 
-// The rng_set_seed() and rng_int() functions form the xoshiro128++ PRNG by
-// David Blackman and Sebastiano Vigna [1]. It is very fast and statistically
-// robust, and it or others of its family are currently the default PRNGs of
-// Javascript, .NET, GNU FORTRAN, Julia and Lua [2].
-//
-// [1] David Blackman and Sebastiano Vigna. Scrambled linear pseudorandom number
-// generators. ACM Trans. Math. Softw., 47:1−32, 2021.
-// [2] Sebastiano Vigna. xoshiro / xoroshiro generators and the PRNG shootout.
-// Retrieved August 2024 from https://prng.di.unimi.it/.
+// The rng() is adapted from the MT19937-64 “Mersenne Twister” pseudorandom
+// number generator (29/09/2004 version), by Takuji Nishimura and Makoto
+// Matsumoto. The original source code is available at
+// http://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/VERSIONS/C-LANG/mt19937-64.c
 
-// Rotate left operation
-#define ROTL(X, K) (((X) << (K)) | ((X) >> (64 - (K))))
+#define NN 312
+#define MM 156
+#define MATRIX_A 0xB5026F5AA96619E9ULL
+#define UM 0xFFFFFFFF80000000ULL // Most significant 33 bits
+#define LM 0x7FFFFFFFULL         // Least significant 31 bits
+#define S 6364136223846793005ULL
 
-static uint64_t s[4];
-
-static uint64_t rng_next_seed(uint64_t seed)
-{
-    uint64_t x = (seed += 0x9e3779b97f4a7c15);
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-}
+// Array for the state vector
+static uint64_t mt[NN];
+// mti == NN + 1 means mt[NN] is not initialized
+static int mti = NN + 1;
 
 void rng_set_seed(uint64_t seed)
 {
-    s[0] = seed;
-    s[1] = rng_next_seed(s[0]);
-    s[2] = rng_next_seed(s[1]);
-    s[3] = rng_next_seed(s[2]);
+    mt[0] = seed;
+    for (mti = 1; mti < NN; ++mti)
+        mt[mti] = S * (mt[mti - 1] ^ (mt[mti - 1] >> 62)) + mti;
 }
 
 // Generate a random 64 bit unsigned int
 uint64_t rng(void)
 {
-    const uint64_t x = ROTL(s[0] + s[3], 23) + s[0];
-    const uint64_t t = s[1] << 17;
+    int i;
+    uint64_t x;
+    static uint64_t mag01[2] = {0ULL, MATRIX_A};
 
-    s[2] ^= s[0];
-    s[3] ^= s[1];
-    s[1] ^= s[2];
-    s[0] ^= s[3];
+    if (mti >= NN) { // Generate NN words at one time
+        // if rng_set_seed() has not been called,
+        // a default initial seed is used
+        if (mti == NN + 1)
+            rng_set_seed(5489ULL);
 
-    s[2] ^= t;
-    s[3] = ROTL(s[3], 45);
+        for (i = 0; i < NN - MM; ++i) {
+            x = (mt[i] & UM) | (mt[i + 1] & LM);
+            mt[i] = mt[i + MM] ^ (x >> 1) ^ mag01[(int)(x & 1ULL)];
+        }
+        for (; i < NN - 1; ++i) {
+            x = (mt[i] & UM) | (mt[i + 1] & LM);
+            mt[i] = mt[i + MM - NN] ^ (x >> 1) ^ mag01[(int)(x & 1ULL)];
+        }
+
+        x = (mt[NN - 1] & UM) | (mt[0] & LM);
+        mt[NN - 1] = mt[MM - 1] ^ (x >> 1) ^ mag01[(int)(x & 1ULL)];
+
+        mti = 0;
+    }
+
+    x = mt[mti++];
+
+    x ^= (x >> 29) & 0x5555555555555555ULL;
+    x ^= (x << 17) & 0x71D67FFFEDA60000ULL;
+    x ^= (x << 37) & 0xFFF7EEE000000000ULL;
+    x ^= (x >> 43);
 
     return x;
 }
